aa09887e525edcf23e098caf1c65ff03bcc9e811
Adam Nobs <nobsadam@gmail.com>
2020-01-02 10:21:31 -0600

    Creation of SuperHeroController

---
+++ SuperHeroStories/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace SuperHeroStories.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SuperHeroStories.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(SuperHeroStories.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
