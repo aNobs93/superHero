755066998f4f77ba46ad92fdf913b298c80526f2
Adam Nobs <nobsadam@gmail.com>
2020-01-02 10:01:05 -0600

    Initial start to project, following along on video to create db search

---
+++ SuperHeroStories/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SuperHeroStories.Startup))]
+namespace SuperHeroStories
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
